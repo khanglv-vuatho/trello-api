dbc9c90259ac18e339bf74b889a8f3af80003337
khanglv-vuatho <khanglv@vuatho.com>
2024-01-24 22:50:51 +0700

    setup router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,15 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRouter } from './boardRoute'
+
+const Router = express.Router()
+
+Router.get('/status', (req, res) => {
+  //check api v1 status
+  res.status(StatusCodes.OK).json({ message: 'api v1 are ready to use' })
+})
+
+//boards APIS
+Router.use('/boards', boardRouter)
+
+export const APIs_V1 = Router
--- src/server.js
+++ src/server.js
@@ -1,32 +1,35 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, CLOSE_DB } from './config/mongodb.js'
+import exitHook from 'async-exit-hook'
+import { env } from './config/environment.js'
+import { APIs_V1 } from './routes/v1/index.js'
 
-const app = express()
+const START_SERVER = async () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.use('/v1', APIs_V1)
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello World, I am running at http://${env.APP_HOST}:${env.APP_PORT}`)
+  })
+
+  exitHook(() => {
+    console.log('Closing database connection')
+    CLOSE_DB()
+    console.log('Exiting')
+  })
+}
+
+;(async () => {
+  try {
+    await CONNECT_DB()
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()

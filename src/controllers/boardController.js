efa14870a4f325b3c7988279db387b52eba2775a
khanglv <khanglv@vuatho.com>
2024-02-29 23:23:47 +0700

    error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -3,12 +3,10 @@ import { StatusCodes } from 'http-status-codes'
 const createNew = (req, res, next) => {
   try {
     console.log(req.body)
-
     res.status(StatusCodes.CREATED).json({ message: 'POST from board controller: create new board' })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
+    //next(error) để đẩy sang errorhandling
+    next(error)
   }
 }
 

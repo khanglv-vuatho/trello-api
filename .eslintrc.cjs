338b5b184486ec00ea151af724e7a5709d752b99
khanglv-vuatho <khanglv>
2024-05-10 11:49:59 +0700

    fix port

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   env: { es2020: true, node: true },
   extends: ['eslint:recommended'],
-  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',

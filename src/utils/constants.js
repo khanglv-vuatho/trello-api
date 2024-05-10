9a19fbbc0d6a173b898505ee7ecddd72e4f0a2db
khanglv-vuatho <khanglv>
2024-05-10 13:49:56 +0700

    update whitelist

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-export const WHITELIST_DOMAINS = ['http://localhost:3000']
+export const WHITELIST_DOMAINS = ['http://localhost:3000', 'trello-api-23wj.onrender.com']
 
 export const BOARD_TYPES = { PUBLIC: 'public', PRIVATE: 'private' }

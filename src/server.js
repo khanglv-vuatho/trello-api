143011dfea7ac6c11205ce8332ebe67a647f0c20
khanglv-vuatho <khanglv>
2024-06-21 21:50:55 +0700

    update'

--- src/server.js
+++ src/server.js
@@ -1,14 +1,10 @@
-import express from 'express'
-import { CONNECT_DB, CLOSE_DB } from './config/mongodb.js'
 import exitHook from 'async-exit-hook'
-import { env } from './config/environment.js'
-import { APIs_V1 } from './routes/v1/index.js'
 import cors from 'cors'
+import express from 'express'
+import { env } from './config/environment.js'
+import { CLOSE_DB, CONNECT_DB } from './config/mongodb.js'
 import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware.js'
-import { corsOptions } from './config/cors.js'
-import multer from 'multer'
-import axios from 'axios'
-import cron from 'node-cron'
+import { APIs_V1 } from './routes/v1/index.js'
 
 const START_SERVER = async () => {
   const app = express()
@@ -23,39 +19,9 @@ const START_SERVER = async () => {
   //middleware xử lí lỗi tập trung
   app.use(errorHandlingMiddleware)
 
-  cron.schedule('*/2 * * * *', async () => {
-    try {
-      // Gửi request tới API
-      const response = await axios.get('https://auto-send-request.onrender.com/')
-
-      // Xử lý dữ liệu trả về nếu cần
-      console.log(response.data)
-    } catch (error) {
-      console.error('Error sending request:', error)
-    }
-  })
-
   app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1>')
     console.log(123)
-  })
-
-  const upload = multer()
-
-  app.post('/', upload.array('files'), async (req, res) => {
-    console.log(req.files)
-
-    if (req.files.length > 0) {
-      req.files.map(async (item) => {
-        const response = await axios.put(`https://match-dev.khangluong2002.workers.dev/${item.originalname}`, item.buffer, {
-          header: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })
-      })
-    }
-
-    res.send({ message: '123' })
   })
 
   if (env.BUILD_MODE === 'production') {

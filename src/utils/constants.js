e44bdd33b8470d8f2db681066a15aca8c985a06b
khanglv-vuatho <khanglv>
2024-10-14 17:34:12 +0700

    replace author

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,3 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-export const WHITELIST_DOMAINS = [
-  'http://localhost:3000',
-  'https://trello-web-yv1v.vercel.app',
-  'https://auto-send-request.onrender.com',
-  'www.trisielts.online',
-  'https://www.trisielts.online'
-]
+export const WHITELIST_DOMAINS = ['http://localhost:3000', 'https://trello-web-yv1v.vercel.app', 'https://auto-send-request.onrender.com']
 
 export const BOARD_TYPES = { PUBLIC: 'public', PRIVATE: 'private' }
